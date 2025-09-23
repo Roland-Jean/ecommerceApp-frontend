f6b4f49fa3ae1725f518314f1c3e20eb200bb13b
Herve176 <hdjomo1@gmail.com>
2025-09-23 17:38:54 +0200

    feature: form validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import{useForm} from "react-hook-form";
 
 export default function Login() {
   const [activeTab, setActiveTab] = useState("login");
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import{useForm} from "react-hook-form";
+
 
 export default function Register() {
   const [step, setStep] = useState(0);
