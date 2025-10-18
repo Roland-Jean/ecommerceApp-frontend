b5d843880a42cc3ff0b4f8522e6a8abff8106a1e
herve <hdjomo1@gmail.com>
2025-10-19 01:14:33 +0200

    feature: backend connection

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081/api/v1",
+  baseURL: "https://ecommerceapp-ii5l.onrender.com/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
---
+++ src/environment/environment-dev.js
@@ -0,0 +1,4 @@
+export default function getEnvironment(){
+  const  baseUrl="https://ecommerceapp-ii5l.onrender.com/api/v1"
+  return {baseUrl}
+}
